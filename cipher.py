39c3ae87b6325de854d4a48dd5e259590a252980
dlewisoconnell <112989604+dlewisoconnell@users.noreply.github.com>
2023-08-02 18:51:33 -0400

    second commit

--- cipher.py
+++ cipher.py
@@ -1,10 +1,19 @@
-def caesar_cipher(text):
+def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
-        encrypted_char = chr(ord(char) + 5)
-        encrypted_text += encrypted_char
+        if char.isalpha():
+            shift_amount = shift % 26  # Ensure shift is within one full rotation of the alphabet
+            if char.islower():
+                base = ord('a')
+            else:
+                base = ord('A')
+            encrypted_char = chr((ord(char) - base + shift_amount) % 26 + base)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
     return encrypted_text
 
 plain_text = input("Please enter a sentence: ")
-encrypted_text = caesar_cipher(plain_text)
+shift = int(input("Please enter the shift value: "))
+encrypted_text = caesar_cipher(plain_text, shift)
 print("The encrypted sentence is:", encrypted_text)
