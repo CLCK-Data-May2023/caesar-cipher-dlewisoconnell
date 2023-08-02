4482ee0d476cd80118e7acb75462ef8b847d0da9
dlewisoconnell <112989604+dlewisoconnell@users.noreply.github.com>
2023-08-02 17:59:47 -0400

    first commit

--- cipher.py
+++ cipher.py
@@ -1,1 +1,10 @@
-# add your code here
+def caesar_cipher(text):
+    encrypted_text = ""
+    for char in text:
+        encrypted_char = chr(ord(char) + 5)
+        encrypted_text += encrypted_char
+    return encrypted_text
+
+plain_text = input("Please enter a sentence: ")
+encrypted_text = caesar_cipher(plain_text)
+print("The encrypted sentence is:", encrypted_text)
